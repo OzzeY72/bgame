02d2c5fe4b833c927eaef546ce4bf712cb619e2a
Maksym Vrutskyi <nabacap72@gmail.com>
2026-09-18 04:50:51 +0200

    fix: text

--- src/data/dialogues/index.ts
+++ src/data/dialogues/index.ts
@@ -76,7 +76,7 @@ export const HANDMADE_DIALOGUES: Record<string, Dialogue> = {
   item_macbook: [
     { think: true, text: 'Ноутбук. Теперь-то валорант лагать не будет' },
     { think: true, text: 'Только вот Riot дауны не адаптировали античит, прийдётся в косынку на нём играть' },
-    { who: 'anastasiia', text: 'Это лучший ненастоящий подарок за сегодня!!' },
+    { who: 'anastasiia', text: 'Это лучший НЕ настоящий подарок за сегодня!!' },
   ],
 
   sewer_gift_locked: [
