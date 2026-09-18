dffbec12bfbc6894a0a26c78c72b65dbb753c83c
Maksym Vrutskyi <nabacap72@gmail.com>
2026-09-18 23:49:50 +0200

    fix: cutscene

--- src/data/cutscenes/index.ts
+++ src/data/cutscenes/index.ts
@@ -69,7 +69,7 @@ export const HANDMADE_CUTSCENES: Record<string, Cutscene> = {
               await p.walkToTile(side, p.tileY);
               await p.walkToTile(side, 6);
             }
-            await p.walkToTile(9, 7);
+            await p.walkToTile(8, 6);
             p.face('up');
           } },
           { sfx: 'open' },
